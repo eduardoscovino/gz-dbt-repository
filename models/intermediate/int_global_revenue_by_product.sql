ef383194409e684b098623f719f105a31f6c2925
Eduardo Scovino <eduardo.scovino@gmail.com>
2023-10-31 13:40:54 +0000

    zzz

--- models/intermediate/int_global_revenue_by_product.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    *
-    , avg_price * quantity as estimated_value
-FROM {{ref("int_stock_sales_by_product")}}
-order by estimated_value desc
--- models/intermediate/int_stock_sales_by_product.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    product_id
-    , SUM(quantity) as quantity
-    , SUM(stock) as stock
-    , AVG(price) as avg_price
-FROM {{ref("int_stock_sales")}}
-GROUP BY product_id
